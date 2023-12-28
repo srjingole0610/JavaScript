0614e4d7f82ab1cd135a55a3335aa57e3456c42c
Suraj Ingole <suraj.ingole0610@gmail.com>
2023-12-28 19:36:24 +0530

    Changes related to Coding Challenge #3

--- Fundamental/script.js
+++ Fundamental/script.js
@@ -494,3 +494,58 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
   console.log('Someone else should drive...');
 }
 ////////////////////////////////////
+
+////////////////////////////////////
+// Coding Challenge #3
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the trophy!
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for a minimum score, as well as multiple else-if blocks 😉
+4. BONUS 2: The minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+TEST DATA: Dolphins score 96, 108, and 89. Koalas score 88, 91, and 110
+TEST DATA BONUS 1: Dolphins score 97, 112, and 101. Koalas score 109, 95, and 123
+TEST DATA BONUS 2: Dolphins score 97, 112, and 101. Koalas score 109, 95, and 106
+
+GOOD LUCK 😀
+*/
+
+// Part 1
+// Calculate the average score for Dolphins and Koalas using the given test data.
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+// Part 2
+// Compare the average scores to determine the winner and handle a draw scenario.
+if (scoreDolphins > scoreKoalas) {
+    console.log(`Dolphins win the trophy 🏆`);
+} else if (scoreKoalas > scoreDolphins) {
+    console.log(`Koalas win the trophy 🏆`);
+} else if (scoreDolphins === scoreKoalas) {
+    console.log(`Both win the trophy! 🏆`);
+} else {
+    console.log('No one wins the trophy 😭');
+}
+
+// BONUS 1
+// Calculate the new average scores for Dolphins and Koalas using the bonus test data.
+const scoreDolphinsNew = (97 + 112 + 80) / 3;
+const scoreKoalasNew = (109 + 95 + 50) / 3;
+console.log(scoreDolphinsNew, scoreKoalasNew);
+
+// Compare the new average scores with a minimum score of 100 for a team to win the trophy.
+if (scoreDolphinsNew > scoreKoalasNew && scoreDolphinsNew >= 100) {
+    console.log('Dolphins win the trophy 🏆');
+} else if (scoreKoalasNew > scoreDolphinsNew && scoreKoalasNew >= 100) {
+    console.log('Koalas win the trophy 🏆');
+} else if (scoreDolphinsNew === scoreKoalasNew && scoreDolphinsNew >= 100 && scoreKoalasNew >= 100) {
+    console.log('Both win the trophy! 🏆');
+} else {
+    console.log('No one wins the trophy 😭');
+}
+////////////////////////////////////
