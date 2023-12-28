66871ba4d4674c9566497656fdc040755a62b83c
Suraj Ingole <suraj.ingole0610@gmail.com>
2023-12-28 13:26:09 +0530

    1.Initial Commit. 2.Added Html and JS file 3.Values and Variable

---
+++ script.js
@@ -0,0 +1,45 @@
+// Declare a variable 'js' and assign the string value 'amazing' to it.
+let js = 'amazing'; 
+
+// Print the result of the multiplication 40 * 12 to the console.
+console.log(40 * 12);
+
+
+////////////////////////////////////
+// Values and Variables
+// Print the string 'jonas' to the console.
+console.log("jonas");
+// Print the number 23 to the console.
+console.log(23);
+// Declare a variable 'firstName' and assign the string value 'suraj' to it.
+let firstName = "suraj";
+// Print the value of the variable 'firstName' to the console.
+console.log(firstName);
+
+// Variable name conventions
+// Avoid using underscores in variable names; prefer camelCase for better readability.
+let jonas_matilda = "JM"; 
+
+// 'function' is a reserved keyword in JavaScript; consider using a different name.
+// let function = 27; // Incorrect usage, choose a different variable name.
+let $function = 27; 
+
+// Use descriptive variable names; 'person' represents a person's name in this context.
+let person = "jonas"; 
+
+// Constants, like PI (the mathematical constant), should be in uppercase with underscores.
+let PI = 3.1415; 
+
+// Use camelCase for multi-word variable names; provide clear names for better code understanding.
+let myFirstJob = "Coder"; // Represents the first job of a person.
+let myCurrentJob = "Teacher"; // Represents the current job of a person.
+
+// Generic variable names like 'job1' and 'job2' are less descriptive; consider using more meaningful names.
+let job1 = "programmer"; 
+let job2 = "teacher"; 
+
+// Print the value of the variable 'myFirstJob' to the console.
+console.log(myFirstJob); 
+
+
+////////////////////////////////////
