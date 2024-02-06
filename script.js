22a551cdb24732f60fe1c4c511833ba52a85401f
Suraj Ingole <suraj.ingole0610@gmail.com>
2024-02-06 20:45:42 +0530

    Selection of all required elements

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// Selects the element in the document with the class 'modal' and assigns it to the variable 'modal'
+const modal = document.querySelector('.modal');
+// Selects the element in the document with the class 'overlay' and assigns it to the variable 'overlay'
+const overlay = document.querySelector('.overlay');
+// Selects the element in the document with the class 'hidden' and assigns it to the variable 'hidden'
+const hidden = document.querySelector('.hidden');
+// Selects the element in the document with the class 'close-modal' and assigns it to the variable 'btnCloseModel'
+const btnCloseModel = document.querySelector('.close-modal');
+// Selects all elements in the document with the class 'show-modal' and assigns them to the variable 'btnOpenModel'
+const btnOpenModel = document.querySelectorAll('.show-modal');
+
+
+
