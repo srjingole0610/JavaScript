1dbb1fbeb3c60a11cafa68cd8529304ea6608d65
Suraj Ingole <suraj.ingole0610@gmail.com>
2024-02-06 19:36:55 +0530

    Implementing the Game to restart

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -6,12 +6,6 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 // Initialize the player's score to 20
 let score = 20;
 
-// Display the secret number on the web page (for debugging purposes)
-document.querySelector('.number').textContent = secretNumber;
-
-// Log the secret number to the console (for debugging purposes)
-console.log(secretNumber);
-
 // Add an event listener to the 'Check' button
 document.querySelector('.check').addEventListener('click', function() {
     // Get the value of the input field, which is the player's guess
@@ -24,6 +18,8 @@ document.querySelector('.check').addEventListener('click', function() {
     // Check if the guess matches the secret number
     else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🥇 Correct Number! 🥇';
+        // Display the secret number on the web page (for debugging purposes)
+        document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem'
     }
@@ -61,4 +57,22 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('body').style.backgroundColor = 'red';
         }
     }
+});
+
+// Add an event listener to the 'Again' button
+document.querySelector('.again').addEventListener('click', function() {
+    // Reset the player's score to 20
+    score = 20;
+    // Set the message to prompt the player to start guessing
+    document.querySelector('.message').textContent = 'Start guessing...';
+    // Reset the score displayed on the web page
+    document.querySelector('.score').textContent = score;
+    // Clear the input field for the player's guess
+    document.querySelector('.guess').value = '';
+    // Hide the secret number displayed on the web page
+    document.querySelector('.number').textContent = '?';
+    // Reset the background color of the webpage to default
+    document.querySelector('body').style.backgroundColor = '#222';
+    // Reset the width of the secret number display box
+    document.querySelector('.number').style.width = '15rem';
 });
