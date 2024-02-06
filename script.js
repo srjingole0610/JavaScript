cc5ea08176d0841292753e22c30179caa3f04b0d
Suraj Ingole <suraj.ingole0610@gmail.com>
2024-02-06 23:08:29 +0530

    Changes related to rolling the DIce

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById ('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+let currentScore = 0;
+diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', function() {
+    const dice = Math.floor(Math.random() * 6) + 1;
+    console.log(dice);
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    if(dice!==1){
+    currentScore +=dice;
+    current0El.textContent = currentScore; 
+    }
+    else{
+
+    }
+});
