3cfcf3b06573762690cca003fd664dd64cb975ec
Suraj Ingole <suraj.ingole0610@gmail.com>
2024-02-06 18:15:09 +0530

    Handling Click Event

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,12 +1,18 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number! 🥇';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number! 🥇';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 13;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 13;
+// console.log(document.querySelector('.guess').value);
 
 
-
+document.querySelector('.check').addEventListener('click'
+, function() {
+    const guess = Number(document.querySelector('.guess').value);
+    if(!guess) {
+        document.querySelector('.message').textContent = '⛔ No Number !';
+    }
+});
