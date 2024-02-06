61c99b85bd3a3e1b5ddf3502c96b9f9e03f00e84
Suraj Ingole <suraj.ingole0610@gmail.com>
2024-02-06 23:24:24 +0530

    Changes related to Setting the active player

--- script.js
+++ script.js
@@ -8,11 +8,16 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
-let currentScore = 0;
 diceEl.classList.add('hidden');
+
+const score = [0,0];
+let activePlayer = 0;
+let currentScore = 0;
 
 btnRoll.addEventListener('click', function() {
     const dice = Math.floor(Math.random() * 6) + 1;
@@ -22,9 +27,13 @@ btnRoll.addEventListener('click', function() {
 
     if(dice!==1){
     currentScore +=dice;
-    current0El.textContent = currentScore; 
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     }
     else{
-
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle("player--active");
+        player1El.classList.toggle("player--active");
     }
 });
