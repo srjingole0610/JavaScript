4cd6ae7761738ed65682eceeee30a69c052aba91
Suraj Ingole <suraj.ingole0610@gmail.com>
2024-02-12 18:49:13 +0530

    Optional Chaining

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 // const flights =
 //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-const weekdays =['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const weekdays =['Sun', 'Mon', 'Tue', 'wed', 'Thu', 'Fri', 'Sat'];
 
 const  openingHours = {
   [weekdays[3]]:{
-    open: 12,
+    open: 8,
     close: 22,
   },
   thu: {
@@ -372,6 +372,27 @@ GOOD LUCK 😀
 
 
 ///////////////////////////////////////
-//Enhanced Object Literals
-console.log(restaurant);
-console.log(openingHours);
+// //Enhanced Object Literals
+// console.log(restaurant);
+// console.log(openingHours);
+
+
+///////////////////////////////////////
+//Optional Chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+const days =['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+for(const day of days){
+  // console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+//Optional Chaining on Methoda
+console.log(restaurant.order?.(0,1) ?? 'Methods not Exists');
+console.log(restaurant.orderRisotto?.(0,1) ?? 'Methods not Exists');
+
+//Arrays
+const users = [{name: 'John', email: 'john@example.com'}];
+console.log(users[0]?.name ?? 'User Array empty');
