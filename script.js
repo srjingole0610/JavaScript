6d7ca11e5053bf42b59d70417828a9faf5691b3d
Suraj Ingole <suraj.ingole0610@gmail.com>
2024-02-13 18:56:35 +0530

    Coding Challenge #1

--- script.js
+++ script.js
@@ -414,7 +414,6 @@
 //     console.log(person.fullName.apply(person1, ['New York', 'USA'])); // Output: John Doe, New York, USA
 //     console.log(person.fullName.apply(person2, ['London', 'UK'])); // Output: Jane Doe, London, UK
 
-
 //     Real-Time Scenario:
 //     A common real-time scenario for using call() and apply() methods is in the context of inheritance and method borrowing.
 //     Consider a scenario where you have a base object with a method, and you want to reuse that method in another object:
@@ -437,134 +436,196 @@
 //     console.log(baseObject.greet.call(person2)); // Output: Hello, Jane
 
 // */
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-    this.bookings.push({ flight:`${this.iataCode}${flightNum}`, name });
-  },
-};
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight:`${this.iataCode}${flightNum}`, name });
+//   },
+// };
 
-lufthansa.book(239, 'Suraj Ingole');
-lufthansa.book(635, 'John Harry');
-console.log(lufthansa);
+// lufthansa.book(239, 'Suraj Ingole');
+// lufthansa.book(635, 'John Harry');
+// console.log(lufthansa);
 
-const eurowings ={
-    airline: 'Eurowings',
-    iataCode: 'EW',
-    bookings: [],
-};
+// const eurowings ={
+//     airline: 'Eurowings',
+//     iataCode: 'EW',
+//     bookings: [],
+// };
 
-const book = lufthansa.book;
-// //Does not support
-// book(23, 'Sarah Williams');
-book.call(eurowings,23,'Sarah Williams');
-console.log(eurowings);
-book.call(lufthansa,243,'Nicolas Smith');
-console.log(lufthansa);
+// const book = lufthansa.book;
+// // //Does not support
+// // book(23, 'Sarah Williams');
+// book.call(eurowings,23,'Sarah Williams');
+// console.log(eurowings);
+// book.call(lufthansa,243,'Nicolas Smith');
+// console.log(lufthansa);
 
-const swiss ={
-    airline: 'Swiss Air Lines',
-    iataCode: 'LX',
-    bookings: [],
-}
+// const swiss ={
+//     airline: 'Swiss Air Lines',
+//     iataCode: 'LX',
+//     bookings: [],
+// }
 
-book.call(swiss,583, 'Mary Cooper');
-console.log(swiss);
+// book.call(swiss,583, 'Mary Cooper');
+// console.log(swiss);
 
-
-const flightData = [583, 'George Cooper'];
-book.apply(swiss,flightData);
-console.log(swiss);
-
+// const flightData = [583, 'George Cooper'];
+// book.apply(swiss,flightData);
+// console.log(swiss);
 
 // ////////////////////////////////////////////////
 
+// ////////////////////////////////////////////////
+// //Bind Methods
 
-////////////////////////////////////////////////
-//Bind Methods
+// /*
+//     In JavaScript, the bind() method is used to create a new function with a specified this value, and optionally, initial arguments. It's particularly useful when you want to set a specific context for a function that will be invoked later.
 
-/*
-    In JavaScript, the bind() method is used to create a new function with a specified this value, and optionally, initial arguments. It's particularly useful when you want to set a specific context for a function that will be invoked later.
+//     const person = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     fullName: function() {
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+//     };
 
-    const person = {
-    firstName: 'John',
-    lastName: 'Doe',
-    fullName: function() {
-        return `${this.firstName} ${this.lastName}`;
-    }
-    };
+//     const printFullName = person.fullName; // Assigning the method to a variable
+//     const boundPrintFullName = printFullName.bind(person); // Binding the 'this' value to 'person'
+//     console.log(boundPrintFullName()); // Output: John Doe
+//     In this example:
+//     We have an object person with properties firstName, lastName, and a method fullName.
+//     When we assign person.fullName to printFullName, printFullName loses its context (this), so calling printFullName() would return undefined undefined.
+//     By using bind(person) on printFullName, we bind the this context to the person object, creating a new function boundPrintFullName.
+//     Now, when we call boundPrintFullName(), it returns the expected result with the correct context.
 
-    const printFullName = person.fullName; // Assigning the method to a variable
-    const boundPrintFullName = printFullName.bind(person); // Binding the 'this' value to 'person'
-    console.log(boundPrintFullName()); // Output: John Doe
-    In this example:
-    We have an object person with properties firstName, lastName, and a method fullName.
-    When we assign person.fullName to printFullName, printFullName loses its context (this), so calling printFullName() would return undefined undefined.
-    By using bind(person) on printFullName, we bind the this context to the person object, creating a new function boundPrintFullName.
-    Now, when we call boundPrintFullName(), it returns the expected result with the correct context.
+//     Real-Time Scenario:
+//     Consider a scenario where you want to add event listeners to HTML elements and handle events with methods defined in a JavaScript object.
+//     const button = document.getElementById('myButton');
+//     const handler = {
+//         message: 'Button clicked!',
+//         handleClick: function(event) {
+//             console.log(this.message);
+//         }
+//     };
+//     button.addEventListener('click', handler.handleClick.bind(handler));
+//     In this scenario:
+//     We have an object handler with a method handleClick that logs a message.
+//     We bind handler to the handleClick method using bind(handler).
+//     When the button is clicked, the handleClick method is invoked with the correct context (handler), and it logs the message defined in the object.
 
-    Real-Time Scenario:
-    Consider a scenario where you want to add event listeners to HTML elements and handle events with methods defined in a JavaScript object.
-    const button = document.getElementById('myButton');
-    const handler = {
-        message: 'Button clicked!',
-        handleClick: function(event) {
-            console.log(this.message);
-        }
-    };
-    button.addEventListener('click', handler.handleClick.bind(handler));
-    In this scenario:
-    We have an object handler with a method handleClick that logs a message.
-    We bind handler to the handleClick method using bind(handler).
-    When the button is clicked, the handleClick method is invoked with the correct context (handler), and it logs the message defined in the object.
+// */
+// const bookEw = book.bind(eurowings);
+// const bookSw = book.bind(swiss);
+// const bookLH = book.bind(lufthansa);
+// bookEw(23, 'Steven Williams');
+// console.log(eurowings);
 
+// const bookEW23 = book.bind(eurowings,23);
+// bookEW23('Steven sharper');
+// console.log(eurowings);
 
+// const bookEW23Suraj = book.bind(eurowings,23,'Suraj');
+// bookEW23Suraj();
+// console.log(eurowings);
+
+// //With Event Listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function(){
+//     console.log(this);
+//     this.planes++;
+//     console.log(this.planes);
+// };
+
+// document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// // Partial application
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+// // addVAT = value => value + value * 0.23;
+
+// console.log(addVAT(100));
+// console.log(addVAT(23));
+// const addTaxRate = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+// const addVAT2 = addTaxRate(0.23);
+// console.log(addVAT2(100));
+// console.log(addVAT2(23));
+
+// ////////////////////////////////////////////////
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favorite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
 */
-const bookEw = book.bind(eurowings);
-const bookSw = book.bind(swiss);
-const bookLH = book.bind(lufthansa);
-bookEw(23, 'Steven Williams');
-console.log(eurowings);
 
-const bookEW23 = book.bind(eurowings,23);
-bookEW23('Steven sharper');
-console.log(eurowings);
+const poll = {
+  question: 'What is your favorite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
 
-const bookEW23Suraj = book.bind(eurowings,23,'Suraj');
-bookEW23Suraj();
-console.log(eurowings);
-
-//With Event Listeners
-lufthansa.planes = 300;
-lufthansa.buyPlane = function(){
-    console.log(this);
-    this.planes++;
-    console.log(this.planes);
+  registerNewAnswer: function () {
+    const answers = prompt(
+      `${this.question}\n${this.options.join('\n')}\n(Write Option Number)`
+    );
+    if (!isNaN(answers) && answers < this.answers.length) {
+      this.answers[answers] += 1;
+      this.displayResults();
+      this.displayResults('string');
+    }
+  },
+  displayResults(type ='array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type ==='string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
 };
 
-document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 
-// Partial application
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
-
-const addVAT = addTax.bind(null, 0.23);
-// addVAT = value => value + value * 0.23;
-
-console.log(addVAT(100));
-console.log(addVAT(23));
-const addTaxRate = function (rate) {
-  return function (value) {
-    return value + value * rate;
-  };
-};
-const addVAT2 = addTaxRate(0.23);
-console.log(addVAT2(100));
-console.log(addVAT2(23));
-
-////////////////////////////////////////////////
+///////////////////////////////////////
