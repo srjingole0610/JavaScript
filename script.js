79524270d67daa7d33187aea202a67af598f12cc
Suraj Ingole <suraj.ingole0610@gmail.com>
2024-02-13 16:46:31 +0530

    Value vs Reference

--- script.js
+++ script.js
@@ -24,3 +24,36 @@ createBooking('LH123',5);
 createBooking('LH123', undefined,1000);
 
 
+////////////////////////////////////////////////////////////////
+//Value vs Reference
+
+const flight = 'LH234';
+const suraj = {
+    name: 'Suraj',
+    age: 25,
+    city: 'Bangalore',
+    passport:245678935
+};
+
+const checkIn = function(flightNum,passenger){
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+    
+    if(passenger.passport === 245678935){
+        alert('Check in')
+    } else {
+        alert('Invalid Passport');
+    }
+}
+
+checkIn(flight,suraj);
+console.log(flight);
+console.log(suraj);
+
+const newPassport = function(person){
+    person.passport = Math.floor(Math.random() *10000000000);
+}
+
+newPassport(suraj);
+checkIn(flight,suraj);
+console.log(suraj);
